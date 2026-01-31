2a951aefa157c4451f01fd6d71a27fb1634f643c
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-02-01 00:08:40 +0400

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
