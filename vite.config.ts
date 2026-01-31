6a27122181d16f0ba0381299ece90db19ccdf901
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-02-01 00:27:08 +0400

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins:[react(), tsconfigPaths()],
-  base:"/duty-to-beauty/",
+  plugins: [react(), tsconfigPaths()],
+  base: "/duty-to-beauty/",
 });
 
