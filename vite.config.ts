7b9d332905787a3f5963fe447ced490ecf6addef
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-02-01 20:50:19 +0400

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   base: "/duty-to-beauty/",
 });
-
